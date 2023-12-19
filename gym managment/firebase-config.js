fbcb7db154d7f968eda7a8162aaa2cd556ed3694
Parthmishra163 <135351926+Parthmishra163@users.noreply.github.com>
2023-12-19 12:07:22 +0530

    Add files via upload

---
+++ gym managment/firebase-config.js
@@ -0,0 +1,15 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBtIsKFtMkEOvoQhKZ1WU57J9Ab-9324ck",
+  authDomain: "gym-managment-system-27956.firebaseapp.com",
+  projectId: "gym-managment-system-27956",
+  storageBucket: "gym-managment-system-27956.appspot.com",
+  messagingSenderId: "1083163169662",
+  appId: "1:1083163169662:web:8ca35fe784a02902e5592f",
+  measurementId: "G-JKE9Z1GXDQ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+  
